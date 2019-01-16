e290f41ba4e1fa8da7710c5f7e7a22b1a80097d0
Alex <alexchizhov90@gmail.com>
2019-01-16 19:24:56 +0300

    Randomized data for test_create_account and multiple runs use these data

--- Tests/test_create_account.py
+++ Tests/test_create_account.py
@@ -1,13 +1,20 @@
 from Parameter_Object.user import User
 import pytest
+import random
+import string
 
+def random_string(maxlen):
+    symbol = string.ascii_letters + string.digits
+    return ''.join([random.choice(symbol) for i in range(random.randrange(1,maxlen))])
+
+def random_digits(maxlen):
+    symbol = string.digits
+    return ''.join([random.choice(symbol) for i in range(random.randrange(1,maxlen))])
 
 testdata = [
-User(tax_id='123456', company='My company', firstname='John10',
-     lastname='Jonas', email='john13@mail.com', password='password'),
-
-User(tax_id='123456', company='My company', firstname='Tom9',
-     lastname='Jonas', email='tom13@mail.com', password='password')
+User(tax_id=random_digits(6), company=random_string(10), firstname=random_string(10),
+     lastname=random_string(6), email=random_string(10)+'@mail.com', password=random_string(8))
+    for i in range(10)
 ]
 
 @pytest.mark.parametrize('user', testdata, ids=[repr(i) for i in testdata])
