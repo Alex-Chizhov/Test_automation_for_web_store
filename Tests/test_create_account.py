0e57c174730966cdc5ccb6d7659e8da5b4cde5ce
Alex <alexchizhov90@gmail.com>
2019-01-16 17:35:07 +0300

    Parameterization of the account creation test

--- Tests/test_create_account.py
+++ Tests/test_create_account.py
@@ -1,14 +1,18 @@
 from Parameter_Object.user import User
+import pytest
 
 
-def test_create_account(appf_new_customer):
-    appf_new_customer.user_account.create_account(User(
-        tax_id='123456',
-        company='My company',
-        firstname='Nick',
-        lastname='Jonas',
-        email='nick20@mail.com',
-        password='password'
-    ))
+testdata = [
+User(tax_id='123456', company='My company', firstname='John10',
+     lastname='Jonas', email='john13@mail.com', password='password'),
+
+User(tax_id='123456', company='My company', firstname='Tom9',
+     lastname='Jonas', email='tom13@mail.com', password='password')
+]
+
+@pytest.mark.parametrize('user', testdata, ids=[repr(i) for i in testdata])
+def test_create_account(appf_new_customer, user):
+    appf_new_customer.user_account.create_account(user)
     username = appf_new_customer.user_account.get_username_from_hp()
-    assert username == 'Nick'
+    appf_new_customer.session.user_logout()
+    assert username == user.firstname
