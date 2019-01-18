526553f2ce2c073b8c5bd172459060be727e527a
Alex <alexchizhov90@gmail.com>
2019-01-18 14:33:55 +0300

    Changed path to extension

--- App/app.py
+++ App/app.py
@@ -8,7 +8,7 @@ class Application:
     def __init__(self, browser):
         if browser == 'chrome':
             self.options = webdriver.ChromeOptions()
-            self.options.add_extension('..\\Extensions\\ChroPath_0_3_4_0.crx')
+            self.options.add_extension('./Extensions/ChroPath_0_3_4_0.crx')
             self.wd = webdriver.Chrome(options=self.options)
         elif browser == 'firefox':
             self.wd = webdriver.Firefox()
