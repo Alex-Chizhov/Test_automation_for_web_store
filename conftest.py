c1e5cd68ffa3d5a23894299bff3bc8b2a5fb641f
Alex <alexchizhov90@gmail.com>
2019-01-18 14:42:00 +0300

    Opening file moved into the fixtures, added global variable for saving data from config.json. Added command line option --config for specify a config file.

--- conftest.py
+++ conftest.py
@@ -5,13 +5,18 @@ import json
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome')
+    parser.addoption('--config', action='store', default='config.json')
 
-with open("../config.json") as f:
-    conf_data = json.load(f)
+conf_data = None
 
 @pytest.fixture(scope='session')
 def appf_admin(request):
+    global conf_data
     browser = request.config.getoption('--browser')
+    conf_file = request.config.getoption('--config')
+    if conf_data == None:
+        with open(conf_file) as f:
+            conf_data = json.load(f)
     fixture = Application(browser)
     fixture.session.login_to_admin_panel(username=conf_data["admin_name"], password=conf_data["admin_password"])
     yield fixture
@@ -20,14 +25,24 @@ def appf_admin(request):
 
 @pytest.fixture(scope='session')
 def appf_new_customer(request):
+    global conf_data
     browser = request.config.getoption('--browser')
+    conf_file = request.config.getoption('--config')
+    if conf_data == None:
+        with open(conf_file) as f:
+            conf_data = json.load(f)
     fixture = Application(browser)
     yield fixture
     fixture.destroy()
 
 @pytest.fixture(scope='session')
 def appf_customer(request):
+    global conf_data
     browser = request.config.getoption('--browser')
+    conf_file = request.config.getoption('--config')
+    if conf_data == None:
+        with open(conf_file) as f:
+            conf_data = json.load(f)
     fixture = Application(browser)
     fixture.session.user_login(email=conf_data["user_email"], password=conf_data["user_password"])
     yield fixture
