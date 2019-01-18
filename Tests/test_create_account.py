e07337550819bdfd1c7403435aa1dc8a7447aa76
Alex <alexchizhov90@gmail.com>
2019-01-18 16:59:38 +0300

    Separate test-data from test_create_account

--- Tests/test_create_account.py
+++ Tests/test_create_account.py
@@ -1,21 +1,6 @@
-from Parameter_Object.user import User
+from test_data.create_account import testdata
 import pytest
-import random
-import string
 
-def random_string(maxlen):
-    symbol = string.ascii_letters + string.digits
-    return ''.join([random.choice(symbol) for i in range(random.randrange(1,maxlen))])
-
-def random_digits(maxlen):
-    symbol = string.digits
-    return ''.join([random.choice(symbol) for i in range(random.randrange(1,maxlen))])
-
-testdata = [
-User(tax_id=random_digits(6), company=random_string(10), firstname=random_string(10),
-     lastname=random_string(6), email=random_string(10)+'@mail.com', password=random_string(8))
-    for i in range(10)
-]
 
 @pytest.mark.parametrize('user', testdata, ids=[repr(i) for i in testdata])
 def test_create_account(appf_new_customer, user):
---
+++ test_data/create_account.py
@@ -0,0 +1,17 @@
+from Parameter_Object.user import User
+import random
+import string
+
+def random_string(maxlen):
+    symbol = string.ascii_letters + string.digits
+    return ''.join([random.choice(symbol) for i in range(random.randrange(1,maxlen))])
+
+def random_digits(maxlen):
+    symbol = string.digits
+    return ''.join([random.choice(symbol) for i in range(random.randrange(1,maxlen))])
+
+testdata = [
+User(tax_id=random_digits(6), company=random_string(10), firstname=random_string(10),
+     lastname=random_string(6), email=random_string(10)+'@mail.com', password=random_string(8))
+    for i in range(10)
+]
