8c8aa18fcb54cc48387e5ec753136141e34ecc25
Alex <alexchizhov90@gmail.com>
2019-02-04 00:41:40 +0300

    The switched checking of the test_add_new_product with command line option. And parameterization this test.

--- Tests/test_add_new_product.py
+++ Tests/test_add_new_product.py
@@ -1,16 +1,15 @@
+import pytest
+from Tests_data.product import testdata
 from Parameter_Object.product import Product
 
 
-def test_add_new_product(appf_admin, db):
-    product_count_before = db.get_product_count()
-    appf_admin.admin_panel.add_new_product(Product(
-        name='test_name',
-        date_from='01.01.2018',
-        date_to='01.01.2020',
-        short_description='short_description',
-        description='description',
-        USD=100
-    ))
-    product_count_after = db.get_product_count()
+@pytest.mark.parametrize('product', testdata, ids=[repr(i) for i in testdata])
+def test_add_new_product(appf_admin, db, check_ui, product):
+    products_before = db.get_product_list()
+    appf_admin.admin_panel.add_new_product(product)
+    product_after = db.get_product_list()
 
-    assert product_count_before + 1 == product_count_after
+    assert len(products_before) +1 == len(product_after)
+    if check_ui:
+        assert sorted(product_after, key=Product.sort_key_name) == \
+               sorted(appf_admin.admin_panel.get_product_list(), key=Product.sort_key_name)
