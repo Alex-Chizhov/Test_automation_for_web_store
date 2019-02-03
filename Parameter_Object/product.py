f2fa364689608e83f055165d8457dc4d667f95e1
Alex <alexchizhov90@gmail.com>
2019-02-04 00:34:23 +0300

    The __eq__ method is supplemented

--- Parameter_Object/product.py
+++ Parameter_Object/product.py
@@ -11,11 +11,18 @@ class Product:
         self.id = id
 
     def __repr__(self):
-        return f"name={self.name}, purchase_price={self.USD} id={self.id},"
+        return f"name={self.name} | USD={self.USD} | id={self.id}"
 
     def __eq__(self, other):
         return self.name == other.name and \
-               self.USD == other.USD
+               self.USD == other.USD and \
+               self.id == other.id or  self.id == None and \
+               self.date_to == other.date_to or self.date_to == None and \
+               self.date_from == other.date_from or self.date_from == None and \
+               self.description == other.description or self.description == None and \
+               self.short_description == other.short_description or self.short_description == None
+
+
 
     def sort_key_name(self):
         return self.name
