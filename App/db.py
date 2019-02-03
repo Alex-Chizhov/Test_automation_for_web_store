1e9fd3ad27ad24cd49df9838162f98dac11bbb10
Alex <alexchizhov90@gmail.com>
2019-02-03 06:05:51 +0300

    Added get_product_list method to DbFixture class

--- App/db.py
+++ App/db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from Parameter_Object.user import User
-
+from Parameter_Object.product import Product
 
 class DbFixture:
 
@@ -22,6 +22,25 @@ class DbFixture:
                 (email, tax_id, company, firstname, lastname) = type_fixed_row
                 list_with_objs.append(User(tax_id=tax_id, company=company, firstname=firstname, lastname=lastname,
                                            email=email,))
+        finally:
+            cursor.close()
+
+        return list_with_objs
+
+
+    def get_product_list(self):
+        list_with_objs = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("""    
+                    SELECT lc_products_info.id, lc_products_info.name, lc_products_prices.USD 
+                    FROM lc_products_info 
+                    INNER JOIN lc_products_prices ON lc_products_info.id=lc_products_prices.id;
+                    """)
+            for row in cursor:
+                type_fixed_row = tuple([el.decode('utf-8') if type(el) is bytearray else int(el) for el in row])
+                (id, name, USD) = type_fixed_row
+                list_with_objs.append(Product(id=id, name=name, USD=USD))
 
         finally:
             cursor.close()
