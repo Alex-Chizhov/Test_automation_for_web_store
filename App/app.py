1b55a7e88c31404636910e4087f2a7e921c11a79
Alex <alexchizhov90@gmail.com>
2019-03-05 15:50:43 +0300

    Changed path to extension

--- App/app.py
+++ App/app.py
@@ -2,13 +2,14 @@ from selenium import webdriver
 from App.session import SessionHelper
 from App.admin_panel import AdminPanelHelper
 from App.user_account import UserAccount
+import os
 
 class Application:
 
     def __init__(self, browser):
         if browser == 'chrome':
             self.options = webdriver.ChromeOptions()
-            self.options.add_extension('./Extensions/ChroPath_0_3_4_0.crx')
+            self.options.add_extension(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..\\Extensions\\ChroPath_0_3_4_0.crx", ))
             self.wd = webdriver.Chrome(options=self.options)
         elif browser == 'firefox':
             self.wd = webdriver.Firefox()
