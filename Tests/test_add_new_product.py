f9aeaa4175da969249cd7e176bc64377245c0f4d
Alex <alexchizhov90@gmail.com>
2019-03-05 20:48:28 +0300

    Added Allure steps for test_add_new_product

--- Tests/test_add_new_product.py
+++ Tests/test_add_new_product.py
@@ -1,15 +1,21 @@
 import pytest
 from Tests_data.product import testdata
 from Parameter_Object.product import Product
+import allure
 
 
 @pytest.mark.parametrize('product', testdata, ids=[repr(i) for i in testdata])
 def test_add_new_product(appf_admin, db, check_ui, product):
-    products_before = db.get_product_list()
-    appf_admin.admin_panel.add_new_product(product)
-    product_after = db.get_product_list()
+    with allure.step("Get list of products from DB"):
+        products_before = db.get_product_list()
+    with allure.step(f"Add product {product} in admin panel"):
+        appf_admin.admin_panel.add_new_product(product)
+    with allure.step("Get list of products from DB again"):
+        product_after = db.get_product_list()
+    with allure.step("Comparison the length of the old products list and new one"):
+        assert len(products_before) +1 == len(product_after)
 
-    assert len(products_before) +1 == len(product_after)
     if check_ui:
-        assert sorted(product_after, key=Product.sort_key_name) == \
-               sorted(appf_admin.admin_panel.get_product_list(), key=Product.sort_key_name)
+        with allure.step("Comparison sorted lists of product-objects with fully info about products"):
+            assert sorted(product_after, key=Product.sort_key_name) == \
+                   sorted(appf_admin.admin_panel.get_product_list(), key=Product.sort_key_name)
