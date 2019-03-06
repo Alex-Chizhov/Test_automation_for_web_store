0af79dcdc704d3d92b76fefc63b8f69bc912a462
Alex <alexchizhov90@gmail.com>
2019-03-06 12:20:28 +0300

    Added some allure-decorators for test_user_login

--- Tests/test_user_login.py
+++ Tests/test_user_login.py
@@ -2,6 +2,9 @@ import allure
 from allure_commons.types import AttachmentType
 
 
+@allure.feature('User login')
+@allure.story('User login to website')
+@allure.severity('blocker')
 def test_user_login(appf_customer):
     username = appf_customer.user_account.get_username_from_hp()
     with allure.step('Make a screenshot'):
