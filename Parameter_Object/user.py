de7c6c5e8b3df2b809bbf26ae54f58052ccf5204
Alex <alexchizhov90@gmail.com>
2019-03-15 00:23:45 +0300

    Added attributes for User class

--- Parameter_Object/user.py
+++ Parameter_Object/user.py
@@ -1,17 +1,22 @@
 class User:
 
     def __init__(self, tax_id=None, company=None, firstname=None, lastname=None, email=None,
-                 password=None):
+                 password=None, address1=None, city=None, phone=None, postcode=None):
         self.tax_id = tax_id
         self.company = company
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
         self.password = password
+        self.address1 = address1
+        self.city = city
+        self.phone = phone
+        self.postcode = postcode
 
     def __repr__(self):
         return f"tax_id={self.tax_id}, company={self.company}, firstname={self.firstname}, " \
-               f"lastname={self.lastname}, email={self.email}, password={self.password}"
+               f"lastname={self.lastname}, email={self.email}, password={self.password}  " \
+               f"address1={self.address1}, city={self.city}, phone={self.phone}, postcode={self.postcode}"
 
     def __eq__(self, other):
         return  self.tax_id == other.tax_id or self.tax_id == None and \
