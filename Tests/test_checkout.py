6fa24c0d856ce5f20da33abee7f73c1336b7c890
Alex <alexchizhov90@gmail.com>
2019-04-08 03:07:11 +0300

    Removed whitespaces

--- Tests/test_checkout.py
+++ Tests/test_checkout.py
@@ -8,14 +8,14 @@ def test_Checkout(appf_new_customer):
     appf_new_customer.shop.add_products_to_cart_from_home_page()
     appf_new_customer.shop.checkout(
         User(
-            tax_id = '123456',
-            company = 'Appple',
-            firstname = 'Bob',
-            lastname = 'Jephson ',
-            email = 'jephson@email.com',
-            password = 'password',
-            address1 = '1515 Main St, Billings, MT 59105,',
-            city = 'Montana',
-            phone = '+1 406-896-9680',
-            postcode = '105043'
+            tax_id='123456',
+            company='Apple',
+            firstname='Bob',
+            lastname='Jephson ',
+            email='jephson@email.com',
+            password='password',
+            address1='1515 Main St, Billings, MT 59105,',
+            city='Montana',
+            phone='+1 406-896-9680',
+            postcode='105043'
          ))
