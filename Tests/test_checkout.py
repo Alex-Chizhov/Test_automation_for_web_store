8adf35a691df44596bc9e825b3f00c40c7af18d0
Alex <alexchizhov90@gmail.com>
2019-04-08 03:20:50 +0300

    Added Allure steps

--- Tests/test_checkout.py
+++ Tests/test_checkout.py
@@ -5,17 +5,19 @@ import allure
 @allure.feature('Checkout')
 @allure.severity('blocker')
 def test_Checkout(appf_new_customer):
-    appf_new_customer.shop.add_products_to_cart_from_home_page()
-    appf_new_customer.shop.checkout(
-        User(
-            tax_id='123456',
-            company='Apple',
-            firstname='Bob',
-            lastname='Jephson ',
-            email='jephson@email.com',
-            password='password',
-            address1='1515 Main St, Billings, MT 59105,',
-            city='Montana',
-            phone='+1 406-896-9680',
-            postcode='105043'
-         ))
+    with allure.step('Add products to cart'):
+        appf_new_customer.shop.add_products_to_cart_from_home_page()
+    with allure.step('Checkout'):
+        appf_new_customer.shop.checkout(
+            User(
+                tax_id='123456',
+                company='Apple',
+                firstname='Bob',
+                lastname='Jephson ',
+                email='jephson@email.com',
+                password='password',
+                address1='1515 Main St, Billings, MT 59105,',
+                city='Montana',
+                phone='+1 406-896-9680',
+                postcode='105043'
+             ))
