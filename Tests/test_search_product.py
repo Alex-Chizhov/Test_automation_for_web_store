7521f136bcff3e71f00e44daa5e2d0647b5aeffb
Alex <alexchizhov90@gmail.com>
2019-04-10 05:02:37 +0300

    Added allure steps to test product search

--- Tests/test_search_product.py
+++ Tests/test_search_product.py
@@ -5,5 +5,7 @@ import allure
 @allure.severity('critical')
 def test_search_product(appf_customer):
     product_name = 'Blue duck'
-    appf_customer.shop.search_product(product_name)
-    assert appf_customer.shop.get_title_of_product_from_product_page().lower() == product_name.lower()
+    with allure.step('Search product'):
+        appf_customer.shop.search_product(product_name)
+    with allure.step('Check that the title of the product is equal to the search phrase'):
+        assert appf_customer.shop.get_title_of_product_from_product_page().lower() == product_name.lower()
