d630860003318a94380e684e666c3adc31731787
Alex <alexchizhov90@gmail.com>
2019-04-13 03:28:27 +0300

    Changed test-checkout. Now it uses parametrization.

--- Tests/test_checkout.py
+++ Tests/test_checkout.py
@@ -1,25 +1,13 @@
-from Parameter_Object.user import User
 import allure
 
 
 @allure.feature('Checkout')
 @allure.severity('blocker')
-def test_Checkout(appf_new_customer):
+def test_Checkout(appf_new_customer, json_users):
+    user = json_users
     with allure.step('Add products to cart'):
         appf_new_customer.shop.add_products_to_cart_from_home_page()
     with allure.step('Checkout'):
-        appf_new_customer.shop.checkout(
-            User(
-                tax_id='123456',
-                company='Apple',
-                firstname='Bob',
-                lastname='Jephson ',
-                email='jephson@email.com',
-                password='password',
-                address1='1515 Main St, Billings, MT 59105,',
-                city='Montana',
-                phone='+1 406-896-9680',
-                postcode='105043'
-             ))
-        with allure.step("Check that message contains string 'successfully completed!'"):
-            assert appf_new_customer.shop.get_message_from_order_success_page().__contains__("successfully completed!")
+        appf_new_customer.shop.checkout(user)
+    with allure.step("Check that message contains string 'successfully completed!'"):
+        assert appf_new_customer.shop.get_message_from_order_success_page().__contains__("successfully completed!")
